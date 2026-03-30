9cc0d12813fb7353b8402607539a3666212906fe
Bucketsmith <20886583+Bucketsmith@users.noreply.github.com>
2026-03-30 20:23:00 +0200

    Enhance AutosavePlugin with Saver initialization
    Initialize Saver and register AutosaveListener on game load.

--- src/astru43/autosave/AutosavePlugin.kt
+++ src/astru43/autosave/AutosavePlugin.kt
@@ -3,10 +3,12 @@ package astru43.autosave
 import com.fs.starfarer.api.BaseModPlugin
 import com.fs.starfarer.api.Global
 
-@Suppress("unused") // This is the mods entry point
+@Suppress("unused") // This is the mod's entry point
 class AutosavePlugin : BaseModPlugin() {
+
     private lateinit var settings: Settings
     private var saver: Saver? = null
+
     override fun onApplicationLoad() {
         super.onApplicationLoad()
         settings = Settings()
@@ -14,14 +16,19 @@ class AutosavePlugin : BaseModPlugin() {
 
     override fun onGameLoad(newGame: Boolean) {
         super.onGameLoad(newGame)
+
+        // Initialize the Saver
         saver = Saver(settings)
+
+        // Add Saver as a transient script and listener
         Global.getSector().addTransientScript(saver)
         Global.getSector().addTransientListener(saver)
+
+        // Register AutosaveListener to catch manual/vanilla saves
         Global.getSector().listenerManager.addListener(AutosaveListener())
     }
 
     override fun afterGameSave() {
         saver?.afterGameSave()
     }
-
 }
